3610a30fff88517ae94698dc8758417367ff789b
V0rtex- <simone.murzilli@gmail.com>
2012-08-16 19:22:46 +0200

    maven configuration

--- SurveyAndroid/src/org/geobricks/survey/EntryActivity.java
+++ SurveyAndroid/src/org/geobricks/survey/EntryActivity.java
@@ -1,46 +1,54 @@
 package org.geobricks.survey;
 
 
+import org.geobricks.survey.bean.QuestionBean;
+import org.geobricks.survey.bean.SurveyBean;
+import org.geobricks.survey.utils.ParserUtils;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class EntryActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.questions);
+//        setContentView(R.layout.questions);
       
-//		SurveyBean surveyBean = ParserUtils.parseJSON(this, R.raw.survey);
-//
-//		ScrollView scrollView = new ScrollView(this);
-//		LinearLayout panel = new LinearLayout(this);
-//		panel.setOrientation(LinearLayout.VERTICAL);
-//		LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT);
-//		llp.setMargins(5, 5, 5, 5);
-//
-//		TextView t = new TextView(this);
-//		t.setText(surveyBean.getId());
-//		panel.addView(t, llp);
-//
-//		t = new TextView(this);
-//		t.setText(surveyBean.getName());
-//		panel.addView(t, llp);
-//
-//		t = new TextView(this);
-//		t.setText(surveyBean.getSurvey_abstract());
-//		panel.addView(t, llp);
-//		
-//		 Log.i("JSON", String.valueOf(surveyBean.getQuestions().size()));
-//
-//		for(QuestionBean question : surveyBean.getQuestions()) {
-//			TextView q = new TextView(this);
-//			q.setText(question.getNumber() + " | " + question.getText() + " | " + question.getQuestionType());
-//			panel.addView(q, llp);
-//		}
-//
-//		scrollView.addView(panel);
-//		setContentView(scrollView);
+		SurveyBean surveyBean = ParserUtils.parseJSON(this, R.raw.survey);
+
+		ScrollView scrollView = new ScrollView(this);
+		LinearLayout panel = new LinearLayout(this);
+		panel.setOrientation(LinearLayout.VERTICAL);
+		LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT);
+		llp.setMargins(5, 5, 5, 5);
+
+		TextView t = new TextView(this);
+		t.setText(surveyBean.getId());
+		panel.addView(t, llp);
+
+		t = new TextView(this);
+		t.setText(surveyBean.getName());
+		panel.addView(t, llp);
+
+		t = new TextView(this);
+		t.setText(surveyBean.getSurvey_abstract());
+		panel.addView(t, llp);
+		
+		 Log.i("JSON", String.valueOf(surveyBean.getQuestions().size()));
+
+		for(QuestionBean question : surveyBean.getQuestions()) {
+			TextView q = new TextView(this);
+			q.setText(question.getNumber() + " | " + question.getText() + " | " + question.getQuestionType());
+			panel.addView(q, llp);
+		}
+
+		scrollView.addView(panel);
+		setContentView(scrollView);
 
     }
 }
